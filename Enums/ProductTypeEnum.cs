6bbe61b0639f175cf1bc61bc27bdd3e39ee709fe
Jaki Rose <Hendarix@gmail.com>
2024-07-23 11:27:59 -0400

    adds note

--- Enums/ProductTypeEnum.cs
+++ Enums/ProductTypeEnum.cs
@@ -5,6 +5,7 @@ namespace _2Good2EatBackendStore.Enums
 {
     public enum ProductTypeEnum
     {
+        //TODO: Convert this to a table instead of an enum JRG.
         [Display(Name = "None")] None = 0
         , [Display(Name = "Candle")] Candle = 1
         , [Display(Name = "Wax Melt")] WaxMelt = 2
